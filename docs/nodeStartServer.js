e4750d586cfa35293228d128b0194144713b25e6
vsDeus <camioane112@gmail.com>
2020-12-03 17:13:18 +0200

    ## Heroku crash hard coded port 2

--- docs/nodeStartServer.js
+++ docs/nodeStartServer.js
@@ -7,7 +7,7 @@ const dir = path.join(__dirname, 'public');
 
 app.use(express.static(dir));
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log(`Listening`);
 })
 
